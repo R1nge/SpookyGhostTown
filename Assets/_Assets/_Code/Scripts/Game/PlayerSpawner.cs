f7691575a0c505985217cf2a549a5bca4178319f
R1nge <r1ngegamedev@gmail.com>
2024-11-02 19:41:57 +0300

    feat: ghost, survivor prefabs; spawner

---
+++ Assets/_Assets/_Code/Scripts/Game/PlayerSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace _Assets._Code.Scripts.Game
+{
+    public class PlayerSpawner : MonoBehaviour
+    {
+        [SerializeField] private NetworkObject ghostPrefab;
+        [SerializeField] private NetworkObject survivorPrefab;
+        private NetworkManager _networkManager;
+
+        private void Start()
+        {
+            _networkManager = NetworkManager.Singleton;
+            _networkManager.OnClientConnectedCallback += OnConnection;
+        }
+
+        private void OnConnection(ulong clientId)
+        {
+            if (_networkManager.IsHost)
+            {
+                SpawnGhost(clientId);
+            }
+            else
+            {
+                SpawnSurvivor(clientId);
+            }
+        }
+
+        public void SpawnGhost(ulong ownerId)
+        {
+            Instantiate(ghostPrefab, transform.position, Quaternion.identity).SpawnWithOwnership(ownerId);
+        }
+        
+        public void SpawnSurvivor(ulong ownerId)
+        {
+            Instantiate(survivorPrefab, transform.position, Quaternion.identity).SpawnWithOwnership(ownerId);
+        }
+    }
+}
