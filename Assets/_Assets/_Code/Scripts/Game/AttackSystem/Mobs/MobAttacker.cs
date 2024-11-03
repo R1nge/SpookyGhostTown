3cc86d3d22791d0dde16da0691571d3c39ff6730
R1nge <r1ngegamedev@gmail.com>
2024-11-03 11:16:08 +0300

    feat: mobs can attack the closest survivor in range

---
+++ Assets/_Assets/_Code/Scripts/Game/AttackSystem/Mobs/MobAttacker.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using _Assets._Code.Scripts.Game.HealthSystem;
+using _Assets._Code.Scripts.Game.HealthSystem.Survivors;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace _Assets._Code.Scripts.Game.AttackSystem.Mobs
+{
+    public class MobAttacker : NetworkBehaviour
+    {
+        [SerializeField] private float attackRange;
+        [SerializeField] private int damage;
+        [SerializeField] private float attackCooldown;
+
+        private void Start()
+        {
+            if (IsServer)
+            {
+                InvokeRepeating(nameof(Attack), attackCooldown, attackCooldown);
+            }
+        }
+
+        public void Attack()
+        {
+            var colliders = Physics.OverlapSphere(transform.position, attackRange);
+            var sorted = colliders.OrderBy(x => (x.transform.position - transform.position).sqrMagnitude);
+            foreach (var collider in colliders)
+            {
+                if (collider.TryGetComponent(out SurvivorHealth survivorHealth))
+                {
+                    survivorHealth.TakeDamage(damage);
+                    break;
+                }
+            }
+        }
+    }
+}
